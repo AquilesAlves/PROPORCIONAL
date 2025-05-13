aefbcf670d76e97e8d0bbadb208dd78768689320
Aquiles Alves <aquilespereira789@gmail.com>
2025-05-13 20:10:48 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function trocaVencimento(params) {
-    
+function trocaVencimento() {
+    const vencimentoAtual = parseInt(document.getElementById('vencimentoAtual').value)
+    const vencimentoNovo = parseInt(document.getElementById('vencimentoNovo').value)
+    const valorPlano = parseFloat(document.getElementById('valorPlano').value)
+    const diasMes = 30
+
+    console.log(vencimentoAtual)
+
+    if (vencimentoAtual > vencimentoNovo) {
+        let diasProporcionais = vencimentoNovo - vencimentoAtual
+    } else {
+        let diasProporcionais = (30 - vencimentoAtual) + vencimentoNovo
+    }
+
+    let diariaPlano = valorPlano / 30
+    let proporcional = diariaPlano * diasProporcionais
+    let valorTotal = proporcional + valorPlano
+
+    alert(valorTotal)
+
 }

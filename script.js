1f26dffb1264ea08a31d34f3466f5bf1c6dbf469
Aquiles Alves <aquilespereira789@gmail.com>
2025-05-16 22:03:59 -0300

    u

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function trocaVencimento() {
     const vencimentoAtual = parseInt(document.getElementById('vencimentoAtual').value)
     const vencimentoNovo = parseInt(document.getElementById('vencimentoNovo').value)
-    const valorPlano = parseFloat(document.getElementById('valorPlano').value)
+    let valorPlano = parseFloat(document.getElementById('valorPlano').value)
     const msg1 = document.getElementById('msg1')
     const msg2 = document.getElementById('msg2')
 
@@ -23,31 +23,24 @@ function trocaVencimento() {
 
     valorTotal = proporcional + valorPlano
 
-    // resultado.style.borderRadius = "15px"
-    // resultado.style.padding = "15px"
-    // resultado.style.height = "170px"
-    // resultado.style.width = "350px"
+    valorTotal = (valorTotal).toFixed(2).replace('.', ',')
+    valorPlano = (valorPlano).toFixed(2).replace('.', ',')
+    proporcional = (proporcional).toFixed(2).replace('.', ',')
 
     
-    // resultado.innerHTML = `
-    // <p><strong>Dias proporcionais:</strong>  ${diasProporcionais}  </p>
-    // <p><strong>Valor do proporcional:</strong> R$  ${(proporcional).toFixed(2).replace('.', ',')}  </p>
-    // <p><strong>Valor da fatura:</strong> R$  ${(valorTotal).toFixed(2).replace('.', ',')}  </p>
-    // `
-
     msg1.style.borderRadius = "15px"
     msg1.style.padding = "15px"
-    msg1.style.height = "200px"
+    msg1.style.height = "130px"
     msg1.style.width = "350px"
     msg1.innerHTML = `
-        <p>Realizando a troca de vencimento do dia ${vencimentoAtual} para o dia ${vencimentoNovo}, o sistema irá calcular o *proporcional* de ${diasProporcionais}dias, e isso dará o valor de ${valorTotal} para data escolhida, e as próximas faturas voltam para o valor real do seu plano que é ${valorPlano}. Posso seguir com a alteração de vencimento?</p>
+        <p>Realizando a troca de vencimento do dia ${vencimentoAtual} para o dia ${vencimentoNovo}, o sistema irá calcular o *proporcional* de ${diasProporcionais} dias, e isso dará o valor de ${valorTotal} para data escolhida, e as próximas faturas voltam para o valor real do seu plano que é ${valorPlano}. Posso seguir com a alteração de vencimento?</p>
     `
     document.getElementById('copy1').style.display = 'block'     //faz aparecer o botao
 
 
     msg2.style.borderRadius = "15px"
     msg2.style.padding = "15px"
-    msg2.style.height = "100px"
+    msg2.style.height = "80px"
     msg2.style.width = "350px"
     msg2.innerHTML = `
         Cliente quis alterar o vencimento de ${vencimentoAtual} para ${vencimentoNovo}
